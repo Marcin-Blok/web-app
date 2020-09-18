38fd52eef007d819a5d9c6eed7cd34cd6a209fc5
Marcin-Blok <marcinblok7980@gmail.com>
2020-09-18 08:37:55 +0200

    Dodanie klasy uruchomieniowej WebApplication

---
+++ src/main/java/pl/marcinblok/webapp/WebApplication.java
@@ -0,0 +1,13 @@
+package pl.marcinblok.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class  WebApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
--- src/main/java/pl/marcinblok/webapp/model/Doctor.java
+++ src/main/java/pl/marcinblok/webapp/model/Doctor.java
@@ -3,17 +3,49 @@ package pl.marcinblok.webapp.model;
 public class Doctor {
 
 
-        private int id;
-        private String name;
-        private String surname;
-        private String pesel;
+    private int id;
+    private String name;
+    private String surname;
+    private String pesel;
 
-        public Doctor() {
-        }
+    public Doctor() {
+    }
 
-        public Doctor(String name, String surname,String pesel){
-            this.name = name;
-            this.surname = surname;
-            this.pesel = pesel;
-        }
+    public Doctor(String name, String surname, String pesel) {
+        this.name = name;
+        this.surname = surname;
+        this.pesel = pesel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
 }

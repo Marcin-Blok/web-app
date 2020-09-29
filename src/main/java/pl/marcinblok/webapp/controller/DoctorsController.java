2e7b91e00eee84bc66630a5b6bd830bc6a94477a
Marcin-Blok <marcinblok7980@gmail.com>
2020-09-29 22:06:26 +0200

    Dodanie pliku form.html

--- src/main/java/pl/marcinblok/webapp/controller/DoctorsController.java
+++ src/main/java/pl/marcinblok/webapp/controller/DoctorsController.java
@@ -1,11 +1,16 @@
 package pl.marcinblok.webapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
-import pl.marcinblok.webapp.DoctorList;
+import pl.marcinblok.webapp.model.Doctor;
+
+import java.util.List;
 
 @Controller
 public class DoctorsController {
@@ -16,8 +21,12 @@ public class DoctorsController {
 
     @GetMapping("/doctors")
     public String allDoctors(Model model) {
-        DoctorList doctors = restTemplate.getForObject("http://localhost:1234/doctors", DoctorList.class);
-        model.addAttribute("doctors", doctors.getDoctors());
+        ResponseEntity<List<Doctor>> doctorResponse =
+                restTemplate.exchange("http://localhost:8080/doctors",
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Doctor>>() {
+                        });
+        List<Doctor> doctors = doctorResponse.getBody();
+        model.addAttribute("doctors", doctors);
         return "doctors";
     }
 }
